82e48bf4c8ef16dea318b7ac9ac756a1b70d3c4c
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2024-09-19 17:54:54 +0530

    Fixed issue Windows SearchBar MaxLength > 0 not working properly

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue5669.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Maui.Controls.Sample.Issues
+{ 
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Issue5669 : ContentPage
+	{
+		public Issue5669()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			searchBar.MaxLengthProperty = 5;
+		}
+	}
+}
