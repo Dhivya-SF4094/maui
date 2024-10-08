48940e7548ce7211024ebf6b4171fff3ce5ddf62
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2024-10-08 16:50:13 +0530

    Changed Height set property to internal set

--- src/Controls/src/Core/VisualElement/VisualElement.cs
+++ src/Controls/src/Core/VisualElement/VisualElement.cs
@@ -586,7 +586,7 @@ namespace Microsoft.Maui.Controls
 		public double Height
 		{
 			get { return _mockHeight == -1 ? (double)GetValue(HeightProperty) : _mockHeight; }
-			set { SetValue(HeightPropertyKey, value); }
+			internal set { SetValue(HeightPropertyKey, value); }
 		}
 
 		/// <summary>
