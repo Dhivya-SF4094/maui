22fb10412df077fde277a31cce4aaa589287f898
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-09-30 15:58:43 +0530

    removed unwanted files

--- src/Controls/tests/TestCases.Shared.Tests/UITest.cs
+++ src/Controls/tests/TestCases.Shared.Tests/UITest.cs
@@ -65,8 +65,8 @@ namespace Microsoft.Maui.TestCases.Tests
 					break;
 				case TestDevice.iOS:
 					config.SetProperty("DeviceName", Environment.GetEnvironmentVariable("DEVICE_NAME") ?? "iPhone Xs");
-					config.SetProperty("PlatformVersion", Environment.GetEnvironmentVariable("PLATFORM_VERSION") ?? "17.2");
-					config.SetProperty("Udid", Environment.GetEnvironmentVariable("DEVICE_UDID") ?? "A90F939F-19C9-45A5-B912-0550B6DB2F6E");
+					config.SetProperty("PlatformVersion", Environment.GetEnvironmentVariable("PLATFORM_VERSION") ?? "18.0");
+					config.SetProperty("Udid", Environment.GetEnvironmentVariable("DEVICE_UDID") ?? "");
 					break;
 				case TestDevice.Windows:
 					var appProjectFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "..\\..\\..\\Controls.TestCases.HostApp");
