ab89dfd7b7b516d040b33ede6ec3e68fc95f7276
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-10-23 15:34:00 +0530

    Moved fix to RectangleHandler

--- src/Core/src/Platform/iOS/MauiShapeView.cs
+++ src/Core/src/Platform/iOS/MauiShapeView.cs
@@ -11,8 +11,6 @@ namespace Microsoft.Maui.Platform
 		public MauiShapeView()
 		{
 			BackgroundColor = UIColor.Clear;
-			// Redraw the shape when bounds change to prevent blur during resizing in container layouts.
-			ContentMode = UIViewContentMode.Redraw;
 		}
 
 		[UnconditionalSuppressMessage("Memory", "MEM0002", Justification = IUIViewLifeCycleEvents.UnconditionalSuppressMessage)]
